623c2eaf95c608b5bcdd76425824ae7aa8106c81
Shubhkarman Singh Rathore <shubhkarman4733@yahoo.in>
2023-04-11 19:56:07 -0700

    adds task lister lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  document.querySelector('form').addEventListener('submit', e => { 
+    e.preventDefault();
+    buildToDo(e.target.new_task_description.value);
+    e.target.reset();
+  });
 });
+
+function buildToDo(todo) {
+  let p = document.createElement('p');
+  let btn = document.createElement('button');
+  btn.addEventListener('click', handleToDo);
+  btn.textContent = 'X';
+  p.textContent = `${todo} `;
+  document.querySelector('#tasks').appendChild(p);
+  p.appendChild(btn);
+}
+
+function handleToDo(e) {
+  e.target.parentNode.remove();
+}
